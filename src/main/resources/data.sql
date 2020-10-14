58af4656661d3398487e5bbcee7676ced3d5c67c
Timur <TamerlanTheTerrible@users.noreply.github.com>
2020-10-14 09:34:42 +0500

    db initialization

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,9 @@
+INSERT INTO public.employee (
+first_name, last_name) VALUES(
+'Alex'::character varying, 'Kim'::character varying)
+ returning id;
+
 INSERT INTO public.user_access (
-email, is_active, password, role, username) VALUES (
-'admin@samarkanda-travel.com'::character varying, true::boolean, 'admin'::character varying, 'ROLE_ADMIN'::character varying, 'admin'::character varying)
+email, is_active, password, role, username, employee_id) VALUES (
+'admin@samarkanda-travel.com'::character varying, true::boolean, 'admin'::character varying, 'ROLE_ADMIN'::character varying, 'admin'::character varying, 1::bigint)
  returning id;

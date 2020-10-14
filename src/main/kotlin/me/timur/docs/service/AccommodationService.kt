b5a581a131a296f5d2eb5bd56bd31790d70307e4
Timur <TamerlanTheTerrible@users.noreply.github.com>
2020-10-14 18:00:01 +0500

    testing with accommodation controller 3: returning index

--- src/main/kotlin/me/timur/docs/service/AccommodationService.kt
+++ src/main/kotlin/me/timur/docs/service/AccommodationService.kt
@@ -26,12 +26,8 @@ class AccommodationService
         return accommodationRepository.findAllByGroup(group)
     }
 
-//    fun findAll(): Map<Group?, List<Accommodation>> {
-//        return accommodationRepository.findAll().groupBy { it.group }
-//    }
-
-    fun findAll(): List<Accommodation> {
-        return accommodationRepository.findAll()
+    fun findAll(): Map<Group?, List<Accommodation>> {
+        return accommodationRepository.findAll().groupBy { it.group }
     }
 
     fun findById(id: Long): Accommodation{

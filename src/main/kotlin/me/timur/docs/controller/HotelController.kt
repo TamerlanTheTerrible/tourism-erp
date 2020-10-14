2ad854087659696ddde3cdc6a931ff98e4b99508
Timur <TamerlanTheTerrible@users.noreply.github.com>
2020-10-14 18:57:08 +0500

    HotelController.kt: test1

--- src/main/kotlin/me/timur/docs/controller/HotelController.kt
+++ src/main/kotlin/me/timur/docs/controller/HotelController.kt
@@ -1,8 +1,6 @@
 package me.timur.docs.controller
 
 import me.timur.docs.repository.AccommodationRepository
-import me.timur.docs.service.AccommodationService
-import me.timur.docs.service.GroupService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -20,14 +18,13 @@ class HotelController
                         )
 {
 
-    @GetMapping("/all","/")
+    @GetMapping("/all", "/")
     fun hotels(model: Model): String {
         val accommodations = accomRepo.findAll()
         model.addAttribute("accommodations", accommodations)
-        val address: String= if (accommodations.isEmpty())
+        return if (accommodations.isEmpty())
             "/index"
         else
             "/tour_operator/accommodation/accommodation"
-        return address
     }
 }
